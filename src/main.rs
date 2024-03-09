af04b3bd1d959f8164d9d1a0dff16c48cbcb54ad
daniel <danielotavianopontes@gmail.com>
2024-03-09 15:46:22 -0300

    feat: add print when init the directory

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,7 @@ fn main() {
             fs::create_dir(".git/objects").unwrap();
             fs::create_dir(".git/refs").unwrap();
             fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
+            println!("Initialized git directory")
         }
     }
 }
