5f16bea18f77b3c0e5fceaa2a8928ba191a467dd
daniel <danielotavianopontes@gmail.com>
2024-03-09 15:44:15 -0300

    feat: add init command

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use std::fs;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Debug, Subcommand, Clone)]
+enum Command {
+    Init,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    match args.command {
+        Command::Init => {
+            fs::create_dir(".git").unwrap();
+            fs::create_dir(".git/objects").unwrap();
+            fs::create_dir(".git/refs").unwrap();
+            fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
+        }
+    }
+}
